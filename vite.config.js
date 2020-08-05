d8663c139ff7ef01e72249f271327e88a1709649
Alpha Tang <alpha.tang@huifu.com>
2020-08-05 23:39:28 +0800

    feat: 修改 vite.confg 打包路径，以满足 github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  outDir: 'docs',
+  assetsDir: 'assets',
   transforms: [
     {
-      test: ({ path }) => path.endsWith(".md"),
+      test: ({ path }) => path.endsWith('.md'),
       transform({ code }) {
         return `export default ${JSON.stringify(code)}`;
       },
